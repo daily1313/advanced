65a3403b036102b2b54bf75d853119f2247ec81b
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2023-08-08 17:41:23 +0900

    feat: 변하지 않는 것과 변하는 것을 구분하기 위한 템블릿 메서드 패턴 적용, 익명 내부 클래스 사용

---
+++ src/main/java/com/example/advanced/trace/template/AbstractTemplate.java
@@ -0,0 +1,28 @@
+package com.example.advanced.trace.template;
+
+import com.example.advanced.trace.TraceStatus;
+import com.example.advanced.trace.logtrace.LogTrace;
+
+public abstract class AbstractTemplate<T> {
+
+    private final LogTrace trace;
+
+    public AbstractTemplate(LogTrace trace) {
+        this.trace = trace;
+    }
+
+    public T execute(String message) {
+        TraceStatus status = null;
+        try {
+            status = trace.begin(message);
+            T result = call();
+            trace.end(status);
+            return result;
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e; // 예외를 꼭 다시 던져주어야 한다.
+        }
+    }
+
+    protected abstract T call();
+}
