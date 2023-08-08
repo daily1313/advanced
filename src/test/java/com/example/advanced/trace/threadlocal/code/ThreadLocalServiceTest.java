24ebcd7ff0b22a4a265357c89c2824b972c578b2
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2023-08-08 16:28:22 +0900

    feat: 동시성 문제를 해결하기 위한 ThreadLocal 적용

---
+++ src/test/java/com/example/advanced/trace/threadlocal/code/ThreadLocalServiceTest.java
@@ -0,0 +1,43 @@
+package com.example.advanced.trace.threadlocal.code;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+class ThreadLocalServiceTest {
+
+    private ThreadLocalService service = new ThreadLocalService();
+
+    @Test
+    void field() {
+        log.info("main Start");
+        Runnable userA = () -> {
+            service.logic("userA");
+        };
+        Runnable userB = () -> {
+            service.logic("userB");
+        };
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("thread-A");
+        Thread threadB = new Thread(userB);
+        threadB.setName("thread-B");
+
+        threadA.start();
+        sleep(100);
+        threadB.start();
+
+        sleep(2000);
+        log.info("main exit");
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
